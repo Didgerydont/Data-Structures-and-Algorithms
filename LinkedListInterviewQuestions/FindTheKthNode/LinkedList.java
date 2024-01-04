6d1234460b4e9979d1daf61a08648991bb29b4a2
Will Prendergast <prendygast@hotmail.com>
2024-01-04 09:08:20 +0000

    Implemented FindTheKthNode solution

---
+++ LinkedListInterviewQuestions/FindTheKthNode/LinkedList.java
@@ -0,0 +1,94 @@
+package LinkedListInterviewQuestions.FindTheKthNode;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (head == null) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("\nLinked List:");
+        if (head == null) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public Node findKthFromEnd(int k) {
+        // Initialize the two pointers
+        Node fast = head;
+        Node slow = head;
+
+        // Move fast pointer k nodes into the list
+        for (int i = 0; i < k; i++) {
+            // If the list has fewer than k nodes, return null
+            if (fast == null) return null;
+            fast = fast.next;
+        }
+
+        // Move both pointers until fast reaches the end
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // Slow is now at the k-th node from the end
+        return slow;
+    }
+
+
+}
